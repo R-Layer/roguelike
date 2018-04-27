145e877c1e37b33aae3c429cf8d407efa058f538
R-Layer <layer@europe.com>
2018-04-27 12:25:00 +0200

    First daily commit

--- src/index.js
+++ src/index.js
@@ -38,12 +38,14 @@ class App extends React.Component {
         this.fogwar = true;
         this.counter = 0;
         this.enemyPositions = [ [120,120], [140,120], [100,120] ];
+        this.weaponPositions = [ [125,270] ];
 
         this.player =  {hp: 30, weapon: 'knife', weaponValue: 8, level:1};
 
-        this.enemies = [ {hp: 10, weapon: 'dagger', weaponValue: 10, level:1}, 
-                         {hp: 10, weapon: 'dagger', weaponValue: 10, level:1}, 
-                         {hp: 10, weapon: 'dagger', weaponValue: 10, level:1} ]
+        this.enemies = [ {hp: 20, weapon: 'dagger', weaponValue: 10, level:1}, 
+                         {hp: 20, weapon: 'dagger', weaponValue: 10, level:1}, 
+                         {hp: 20, weapon: 'dagger', weaponValue: 10, level:1} ]
+        this.weapons = [ {name: 'double-handed sword', value: 30} ]
     }
 
 componentDidMount() {
@@ -95,6 +97,9 @@ drawBase = () => {
     this.ctx.fillStyle = "rgba(250, 0, 0, 1)";
     this.enemyPositions.map ( el => this.ctx.fillRect(el[0], el[1], 5, 5) );
 
+    this.ctx.fillStyle = "rgba(10, 100, 200, 1)";
+    this.weaponPositions.map (el => this.ctx.fillRect(el[0], el[1], 5, 5) );
+
     this.ctx.fillStyle = "rgba(0, 0, 250, 1)"
     if (this.door)
         this.ctx.fillRect(90, 215, 70, 5);
@@ -114,7 +119,7 @@ checkCollision = (obstacle, ctx) => {
    
 
     const test = obstacle.data.filter( el => el !== 255);
-
+    console.log(test);
     switch(true) 
     {
         case test.length === 0:
@@ -126,11 +131,44 @@ checkCollision = (obstacle, ctx) => {
             this.dy = this.newY;
             this.enemyEncounter();
                 return;
+        case test[0] === 10:
+            this.newX = this.dx;
+            this.newY = this.dy;
+            this.weaponCollection();
+            break;
+        case test[0] === 50:
+            this.newX = this.dx;
+            this.newY = this.dy;
+            this.healtCollection();
+            break;
         default:
             this.dx = this.newX;
             this.dy = this.newY;
                 return;
     }
+}
+
+
+healtCollection = () => {
+    return this.player.hp += 25;
+}
+
+weaponCollection = () => {
+    let weaponIndex = 0;
+    this.weaponPositions.forEach( (el, ind) => 
+    {
+    if ( (38 + this.dx < el[0] && 52 + this.dx > el[0])
+    &&
+      (68 + this.dy < el[1] && 82 + this.dy > el[1]) ) 
+     weaponIndex =  ind;
+   });
+   
+   this.player.weapon = this.weapons[weaponIndex].name;
+   this.player.weaponValue = this.weapons[weaponIndex].value;
+
+   this.weaponPositions.splice(weaponIndex, 1);
+
+   return ;
 }
 
 enemyEncounter = () => {
@@ -145,27 +183,30 @@ enemyEncounter = () => {
     });
 
     this.fight(this.player, this.enemies[enemyIndex])
+    if (this.enemies.length === 0) 
+        this.door = false;
 }
 
 
 battleTurn = (attacker, defender) => {
-    defender.hp -= attacker.weaponValue + attacker.weaponValue/10 + attacker.level*2; 
+    defender.hp -= Math.floor(attacker.weaponValue + (Math.random()*5*attacker.weaponValue/10) + attacker.level*2); 
     return;
 }
 
 
-
 fight = (player, enemy) => {
 
-    console.log(player.weaponValue);
+    console.log(player.hp);
+    console.log(enemy.hp);
 
  this.battleTurn(player, enemy);
  if (enemy.hp > 0)
     this.battleTurn(enemy, player);
 else 
       {
-        this.enemyPositions.splice(this.enemyPositions.indexOf(enemy), 1);
-        this.enemies.splice(this.enemyPositions.indexOf(enemy), 1);
+        let index = this.enemies.indexOf(enemy);
+        this.enemyPositions.splice(index, 1);
+        this.enemies.splice(index, 1);
       }
 
  return ;
